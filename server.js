761cb6ec428c30071662b09d831a2660a2411637
Meero Harootunian <meero.harootunian@gmail.com>
2019-11-20 18:21:07 -0700

    added router, set template engine to handlebars

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const hb = require("express-handlebars");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded( {extended: true} ));
+app.use(express.urlencoded( {extended: true} ));    
+
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started at ${server.address().address}:${server.address().port}`);
+});
